39b4cae8d8b9a0b4df8e83252a7fe6ec51fa1347
Matthew Kennedy <m.kennedy@me.com>
2018-11-18 03:07:54 +0000

    improve more

--- app/overrides/add_google_add_to_cart_to_orders_edit.rb
+++ app/overrides/add_google_add_to_cart_to_orders_edit.rb
@@ -1,9 +1,9 @@
 if Gem.loaded_specs['spree_core'].version >= Gem::Version.create('3.5.0')
   Deface::Override.new(
     virtual_path: 'spree/orders/edit',
-    name: 'add_google_begin_checkout_to_orders_edit',
+    name: 'add_google_add_to_cart_to_orders_edit',
     insert_top: '[data-hook="cart_container"]',
-    partial: 'spree/shared/trackers/google/google_begin_checkout',
-    disabled: false
+    partial: 'spree/shared/trackers/google/google_add_to_cart',
+    disabled: true
   )
 end
