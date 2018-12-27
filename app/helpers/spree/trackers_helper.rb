cd3684b73d09dbaf2de5f49f4e893ee1b52cad2b
Matthew Kennedy <m.kennedy@me.com>
2018-12-27 21:29:20 +0000

    insert

--- app/helpers/spree/trackers_helper.rb
+++ app/helpers/spree/trackers_helper.rb
@@ -1,6 +1,15 @@
 module Spree
   module TrackersHelper
 
+    def product_for_google(product, optional = {})
+      {
+        id: product.sku,
+        name: product.name,
+        price: product.price,
+      }.tap do |hash|
+      end.merge(optional).to_json.html_safe
+    end
+
     def product_for_segment(product, optional = {})
       {
         product_id: product.sku,
